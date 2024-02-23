e594616d648f8496ba46678426d213e2cbd56044
cbh123 <charlieholtz@gmail.com>
2024-02-23 10:05:59 -0800

    latest

--- lib/emoji_web/live/home_live.ex
+++ lib/emoji_web/live/home_live.ex
@@ -11,8 +11,8 @@ defmodule EmojiWeb.HomeLive do
      |> assign(local_user_id: nil)
      |> assign(remove_bg: false)
      |> stream(:my_predictions, [])
-     |> stream(:featured_predictions, Predictions.list_latest_predictions(9))
-     |> stream(:latest_predictions, Predictions.list_latest_safe_predictions(9))}
+     |> stream(:featured_predictions, Predictions.list_featured_predictions())
+     |> stream(:latest_predictions, Predictions.list_latest_predictions(9))}
   end
 
   def handle_event("thumbs-up", %{"id" => id}, socket) do
