2e5994a1e2457a27b9a7d25628419cb822246323
cbh123 <charlieholtz@gmail.com>
2024-02-24 10:40:38 -0800

    bake stickers

--- lib/sticker/seo.ex
+++ lib/sticker/seo.ex
@@ -5,8 +5,8 @@ defmodule StickerWeb.SEO do
     site: &__MODULE__.site_config/1,
     open_graph:
       SEO.OpenGraph.build(
-        description: "Make stickers with AI",
-        site_name: "Sticker Maker",
+        description: "Bake stickers with AI",
+        site_name: "StickerBaker",
         locale: "en_US",
         image: "/og.webp"
       ),
@@ -20,8 +20,8 @@ defmodule StickerWeb.SEO do
 
   def site_config(conn) do
     SEO.Site.build(
-      default_title: "Sticker Maker",
-      description: "Make stickers with AI",
+      default_title: "StickerBaker",
+      description: "Bake stickers with AI",
       theme_color: "#663399",
       windows_tile_color: "#663399",
       mask_icon_color: "#663399"
