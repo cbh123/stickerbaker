29dee77ce08d4c89e136740c48d8eaccd25034c2
cbh123 <charlieholtz@gmail.com>
2024-02-25 14:01:53 -0800

    input

--- lib/sticker_web/components/core_components.ex
+++ lib/sticker_web/components/core_components.ex
@@ -419,8 +419,8 @@ defmodule StickerWeb.CoreComponents do
         id={@id || @name}
         value={Phoenix.HTML.Form.normalize_value(@type, @value)}
         class={[
-          "mt-2 block w-full rounded-lg border-zinc-300 py-3 px-3 text-xl",
-          "text-zinc-900 focus:outline-none focus:ring-4 sm:text-sm sm:leading-6",
+          "mt-2 block w-full rounded-lg border-zinc-300 py-2.5 sm:py-3 px-3",
+          "text-zinc-900 focus:outline-none focus:ring-4 sm:leading-6",
           "phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400 phx-no-feedback:focus:ring-zinc-800/5",
           "border-zinc-300 focus:border-zinc-400 focus:ring-zinc-800/5",
           @errors != [] && "border-rose-400 focus:border-rose-400 focus:ring-rose-400/10",
